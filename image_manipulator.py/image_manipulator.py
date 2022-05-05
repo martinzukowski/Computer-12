240500ea079d802a4168eb7b3655fd09bef30854
martinzukowski <102551533+martinzukowski@users.noreply.github.com>
2022-05-05 10:35:12 -0700

    Add files via upload

--- image_manipulator.py/image_manipulator.py
+++ image_manipulator.py/image_manipulator.py
@@ -17,32 +17,32 @@ for f in os.listdir('.'):
         i.thumbnail(size_300)
         i.save('300/{}_300{}'.format(fn,fext))
 
-image1= Image.open('apple.jpeg')
-image1.rotate(90).show('apple.jpeg')
+image1= Image.open(r'pics\image_manipulator.py\apple.jpeg')
+image1.rotate(90).show(r'pics\image_manipulator.py\apple.jpeg')
 
-image2 = Image.open('hoop.jpeg')
-image2.convert(mode = 'L').show("hoop.jpeg")
+image2 = Image.open(r'pics\image_manipulator.py\hoop.jpeg')
+image2.convert(mode = 'L').show(r'pics\image_manipulator.py\hoop.jpeg')
 
-image10 = Image.open('tree.jpeg')
-image10.rotate(180).show('tree.jpeg')
+image10 = Image.open(r'pics\image_manipulator.py\tree.jpeg')
+image10.rotate(180).show(r'pics\image_manipulator.py\tree.jpeg')
 
-image11 = Image.open('snail.jpeg')
-image11.filter(ImageFilter.GaussianBlur(10)).show('snail.jpeg')
+image11 = Image.open(r'pics\image_manipulator.py\snail.jpeg')
+image11.filter(ImageFilter.GaussianBlur(10)).show(r'pics\image_manipulator.py\snail.jpeg')
 
-image12 = Image.open('car.jpg')
-image12.convert(mode = 'L').show('car.jpg')
+image12 = Image.open(r'pics\image_manipulator.py\car.jpg')
+image12.convert(mode = 'L').show(r'pics\image_manipulator.py\car.jpg')
 
-image13 = Image.open('school.jpg')
-image13.rotate(20).show('school.jpg')
+image13 = Image.open(r'pics\image_manipulator.py\school.jpg')
+image13.rotate(20).show(r'pics\image_manipulator.py\school.jpg')
 
-image14 = Image.open('phone.jpg')
-image14.filter(ImageFilter.GaussianBlur(15)).show('phone.jpg')
+image14 = Image.open(r'pics\image_manipulator.py\phone.jpg')
+image14.filter(ImageFilter.GaussianBlur(15)).show(r'pics\image_manipulator.py\phone.jpg')
 
-image15 = Image.open('flower.jpg')
-image15.convert(mode = 'L').show('flower.jpg')
+image15 = Image.open(r'pics\image_manipulator.py\flower.jpg')
+image15.convert(mode = 'L').show(r'pics\image_manipulator.py\flower.jpg')
 
-image16 = Image.open('chair.jpg')
-image16.rotate(90).show('chair.jpg')
+image16 = Image.open(r'pics\image_manipulator.py\chair.jpg')
+image16.rotate(90).show(r'pics\image_manipulator.py\chair.jpg')
 
-image17 = Image.open('person.jpg')
-image17 .show('person.jpg')
+image17 = Image.open(r'pics\image_manipulator.py\person.jpg')
+image17 .show(r'pics\image_manipulator.py\person.jpg')
