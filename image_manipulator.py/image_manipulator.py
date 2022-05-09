6dbe05c6d56669bfa3510c9048a6772b44eb8cc7
martinzukowski <102551533+martinzukowski@users.noreply.github.com>
2022-05-09 13:06:33 -0700

    Add files via upload

--- image_manipulator.py/image_manipulator.py
+++ image_manipulator.py/image_manipulator.py
@@ -17,17 +17,17 @@ for f in os.listdir('.'):
         i.thumbnail(size_300)
         i.save('300/{}_300{}'.format(fn,fext))
 
-image1= Image.open(r'pics\image_manipulator.py\apple.jpeg')
-image1.rotate(90).show(r'pics\image_manipulator.py\apple.jpeg')
+image1= Image.open(r'pics\image_manipulator.py\apple.jpg')
+image1.rotate(90).show(r'pics\image_manipulator.py\apple.jpg')
 
-image2 = Image.open(r'pics\image_manipulator.py\hoop.jpeg')
-image2.convert(mode = 'L').show(r'pics\image_manipulator.py\hoop.jpeg')
+image2 = Image.open(r'pics\image_manipulator.py\hoop.jpg')
+image2.convert(mode = 'L').show(r'pics\image_manipulator.py\hoop.jpg')
 
-image10 = Image.open(r'pics\image_manipulator.py\tree.jpeg')
-image10.rotate(180).show(r'pics\image_manipulator.py\tree.jpeg')
+image10 = Image.open(r'pics\image_manipulator.py\tree.jpg')
+image10.rotate(180).show(r'pics\image_manipulator.py\tree.jpg')
 
-image11 = Image.open(r'pics\image_manipulator.py\snail.jpeg')
-image11.filter(ImageFilter.GaussianBlur(10)).show(r'pics\image_manipulator.py\snail.jpeg')
+image11 = Image.open(r'pics\image_manipulator.py\snail.jpg')
+image11.filter(ImageFilter.GaussianBlur(10)).show(r'pics\image_manipulator.py\snail.jpg')
 
 image12 = Image.open(r'pics\image_manipulator.py\car.jpg')
 image12.convert(mode = 'L').show(r'pics\image_manipulator.py\car.jpg')
@@ -46,3 +46,53 @@ image16.rotate(90).show(r'pics\image_manipulator.py\chair.jpg')
 
 image17 = Image.open(r'pics\image_manipulator.py\person.jpg')
 image17 .show(r'pics\image_manipulator.py\person.jpg')
+
+j_list = ['apple', 'hoop', 'chair', 'snail', 'person', 'car', 'flower', 'phone', 'tree','school']
+a_list = ['rotate', 'resize', 'png', 'blur', 'black and white']
+def display_list(x):
+    for i in x:
+        print(i)
+    print('')
+def run():
+    while True:
+        print ("Images: ")
+        display_list(j_list)
+        choice = input("Select image to modify: ")
+        if choice.lower() in j_list:
+            userImage = Image.open(f"{choice}.jpg")
+            userImage.show()
+            if userconfirm() == True:
+                break
+            else:
+                print("Invalid Input\n")
+        alterimage()
+
+def userconfirm():
+    choice = input ('Edit this image? y/n: ').lower()
+    if choice == "y":
+        return True
+    elif choice == 'n':
+        print("")
+        run()
+    else:
+        print('invalid input')
+
+def alterimage():
+    while True:
+        print ("Alter options: ")
+        display_list(a_list)
+        choice = input('Select alter mode: ')
+        choice.lower()
+        if choice in a_list:
+            if userconfirm() == True:
+                if choice == a_list[0]:
+                    rotate()
+                if choice == a_list[1]:
+                    resize()
+                if choice == a_list[2]:
+                    png()
+                if choice == a_list[3]:
+                    blur()
+                if choice == a_list[4]:
+                    blacknwhite()
+            
